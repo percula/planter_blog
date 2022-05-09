e37fadc23536c6a12fff1ffc4a6551cd51e71617
peter <16809252+percula@users.noreply.github.com>
2022-05-08 20:02:40 -0400

    Add affiliate links automatically

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const Gulp = require("gulp");
+const URL = require("url");
+const Cheerio = require("gulp-cheerio");
+
+var tag = "planter05-20";
+
+Gulp.task("affiliate-links", () => {
+    return Gulp.src(["public/posts/**/*.html"])
+    .pipe(Cheerio(function($, file) {
+        var url;
+        $("a").each(function() {
+            var a = $(this);
+            url = URL.parse(a.attr("href"));
+            if(a.attr("href").startsWith("https://www.amazon.com/") && !a.attr("href").includes("?tag=")) {
+                a.attr("href", url.protocol + "//" + url.hostname + url.pathname + "?tag=" + tag);
+            }
+        });
+    }))
+    .pipe(Gulp.dest(function (file) {
+            return file.base;
+        }));
+});
+
+Gulp.task("build", Gulp.series("affiliate-links"));
